e25899f61fab07a5024f90f4a8897b470bf3a4c1
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-09-22 01:54:45 +0530

    more tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,27 @@ function App() {
         <div className='bg-blue-500'>hello</div>
         <div className='bg-yellow-500'>hello</div>
       </div>
+
+
+      <div className='grid grid-cols-6'>
+        <div className='bg-red-500 col-span-4'>hello</div>
+        <div className='bg-green-500'>hello</div>
+        <div className='bg-blue-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+      </div>
+
+
+      <div className='grid grid-cols-6'>
+        <div className='bg-red-500 col-span-6 md:col-span-2'>hello</div>
+        <div className='bg-blue-500 col-span-6 md:col-span-2'>hello</div>
+        <div className='bg-green-500 col-span-6 md:col-span-2'>hello</div>
+
+      </div>
     </>
   )
 }
