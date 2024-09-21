0dd0fe6a557ea118ba92c5583dd285b51b4f1e3f
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-09-21 19:00:57 +0530

    tailwind first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex justify-around'>
+        <div className='bg-red-500'>hello</div>
+        <div className='bg-blue-500'>hello</div>
+        <div className='bg-yellow-500'>hello</div>
+      </div>
+    </>
+  )
+}
+
+export default App
